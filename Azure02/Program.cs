608d9970367d70c511d457a975ab1160a311cd3b
Aresla <areslas@list.ru>
2023-12-20 11:17:33 +0200

    add controller

--- Azure02/Program.cs
+++ Azure02/Program.cs
@@ -1,6 +1,14 @@
+using Azure02.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<Myazure02Context>(options =>
+options.UseSqlServer(
+    "Server=tcp:server-azure02.database.windows.net,1433;Initial Catalog=myazure02;Persist Security Info=False;User ID=tatiana;Password=543210aZ!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    ));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +31,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// dotnet ef dbcontext scaffold 'Server=tcp:server-azure02.database.windows.net,1433;Initial Catalog=myazure02;Persist Security Info=False;User ID=tatiana;Password=543210aZ!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;' Microsoft.EntityFrameworkCore.SqlServer -o Models
